3448bb2dfc0d1c861a658811456266a6708a4961
Michael Koukoullis <mkoukoullis@gmail.com>
2014-03-31 15:46:02 +1100

    Initial project commit

---
+++ FootyTips/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 31/03/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FootyTips/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 31/03/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
