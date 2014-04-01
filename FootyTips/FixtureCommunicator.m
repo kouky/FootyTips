3a58a54612ef18c0b0c0b2732d69a649ace818e7
Michael Koukoullis <mkoukoullis@gmail.com>
2014-04-01 17:02:24 +1100

    Boilerplate FixtureCommunicator class and spec

---
+++ FootyTips/FixtureCommunicator.m
@@ -0,0 +1,13 @@
+//
+//  FixtureCommunicator.m
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 1/04/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import "FixtureCommunicator.h"
+
+@implementation FixtureCommunicator
+
+@end
