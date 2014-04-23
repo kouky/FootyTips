10754668697652df0d7857458d1071d61403cec4
Michael Koukoullis <mkoukoullis@gmail.com>
2014-04-23 22:08:47 +1000

    Test harness InspectableTipsListViewController

---
+++ FootyTipsTests/InspectableTipsListViewController.h
@@ -0,0 +1,16 @@
+//
+//  InspectableTipsListViewController.h
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 23/04/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TipsListViewController.h"
+
+@interface InspectableTipsListViewController : TipsListViewController
+
+- (TipsListManager *)manager;
+
+@end
