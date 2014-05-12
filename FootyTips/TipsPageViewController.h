54918f115bf8f6a21ccfa2a2025062f8398df907
Michael Koukoullis <mkoukoullis@gmail.com>
2014-05-12 17:21:26 +1000

    Revert "Nuke TipsPageViewController"
    This reverts commit e943129da78ac98089b33b882bdc6f4b6ca2307d.

---
+++ FootyTips/TipsPageViewController.h
@@ -0,0 +1,17 @@
+//
+//  TipsPageViewController.h
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 5/05/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TipsManagerDelegate.h"
+
+@class TipsManager;
+@class FootyFixture;
+
+@interface TipsPageViewController : UIPageViewController <UIPageViewControllerDataSource>
+
+@end
