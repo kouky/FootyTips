3e63bfefe14615e68f513e62738dcf318ac2a1a8
Michael Koukoullis <mkoukoullis@gmail.com>
2014-05-30 19:40:23 +1000

    Initial LadderRootViewController

---
+++ FootyTips/LadderRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  LadderRootViewController.h
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 30/05/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LadderRootViewController : UIViewController
+
+@end
---
+++ FootyTips/LadderRootViewController.m
@@ -0,0 +1,38 @@
+//
+//  LadderRootViewController.m
+//  FootyTips
+//
+//  Created by Michael Koukoullis on 30/05/2014.
+//  Copyright (c) 2014 Michael Koukoullis. All rights reserved.
+//
+
+#import "LadderRootViewController.h"
+
+@interface LadderRootViewController ()
+
+@end
+
+@implementation LadderRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
